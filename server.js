8a95fd6db4c7153d1cc13a043458f8bdce947782
clement <clement@evaneos.com>
2014-10-10 10:02:22 +0200

    Added apiResponse object

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+var apiResponse   = require('./lib/apiResponse');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -13,13 +15,20 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
+
+mongoose.connect('mongodb://node:node@localhost:27017');
+
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    var result = { message: 'hooray! welcome to our api!' };
+    var response = new apiResponse();
+    response.setSuccess (result);
+
+    res.json(response.getJson());
 });
 
 // more routes for our API will happen here
